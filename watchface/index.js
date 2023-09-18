b450f57addb59dbc5d706b944fce1d35b110fbc8
Kevedly <evkachmala@gmail.com>
2023-09-18 21:52:26 +0700

    wa

--- watchface/index.js
+++ watchface/index.js
@@ -8,33 +8,50 @@ function fontArray(pattern, amount=10) {
     return res
 }
 
+function createTime(font) {
+    return createWidget(widget.IMG_TIME, {
+        hour_zero: 1,
+        hour_startX: 100,
+        hour_startY: 70,
+        hour_array: font,
+        hour_space: 5,
+        hour_unit_en: font[10],
+        hour_unit_tc: font[10],
+        hour_unit_sc: font[10],
+        minute_follow: 1,
+        minute_zero: 1,
+        minute_array: font,
+        minute_space: 8,
+    })
+}
+
+function createDate(font) {
+    return createWidget(widget.IMG_DATE, {
+        year_startX: 110,
+        year_startY: 80,
+        year_space: 1, // Spacing of text.
+        year_zero: 1, // Whether to make up zeroes.
+        year_follow: 1, // Whether to follow.
+        year_en_array: font,
+        year_sc_array: font,
+        year_tc_array: font,
+        year_is_character: true
+    })
+}
+
 WatchFace({
     initView() {
         const timeArray = fontArray("timeFont/{}.png")
+        timeArray.push("timeFont/10.png")
+        const dateArray = fontArray("dateFont/{}.png")
 
         const bg = createWidget(widget.IMG, {
             x:0, y:0, w:390, h:450,
             src: "bg/0.png"
         })
 
-        const time = createWidget(widget.IMG_TIME, {
-            hour_zero: 1, // Whether to make up zero.
-            hour_startX: 205,
-            hour_startY: 184,
-            hour_array: timeArray,
-            hour_space: 8, // The interval between each array.
-            // Units
-            hour_align: hmUI.align.LEFT,
-            // minute second Replaces hour.
-            minute_follow: 1, // Whether to follow.
-            second_follow: 1, // Whether to follow.
-            // omitted as above
-            am_x: 200,
-            am_x: 200, am_y: 100,
-            am_sc_path: 'am.png',
-            am_en_path: 'am_en.png'
-            // pm as above. Prefix changed from am to pm.
-        })
+        const time = createTime(timeArray)
+        const date = createDate(dateArray)
 
     },
 
